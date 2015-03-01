8de5fe0bf4e47309e4e8081e205fc8af7c73200e
yongaro <>
2015-03-01 18:16:51 +0100

    Parse XML fonctionnel mais incomplet

--- Pokemon-core/src/pokemon/launcher/DesktopLauncher.java
+++ Pokemon-core/src/pokemon/launcher/DesktopLauncher.java
@@ -1,13 +1,31 @@
 package pokemon.launcher;
 
+import pokemon.modele.Atk;
+import pokemon.modele.Capacite;
+import pokemon.modele.Statut;
+import pokemon.modele.Type;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.XmlReader;
+import com.badlogic.gdx.utils.XmlReader.Element;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] arg) throws Exception {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
 		config.width=900;
 		config.height=600;
+		TestCombat.fromXML();
 	}
+	
+	
+
+	
+	
+	
+	
 }
--- Pokemon-core/src/pokemon/launcher/TestCombat.java
+++ Pokemon-core/src/pokemon/launcher/TestCombat.java
@@ -1,58 +1,48 @@
 package pokemon.launcher;
 
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-
-
-
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.XmlReader;
 import com.badlogic.gdx.utils.XmlReader.Element;
+
+
+
+
+
+
 
 import pokemon.modele.*;
 
 
 public class TestCombat {
 	
-	public static void main(String args[]){
+	//Test de parse XML
+	public static void fromXML() throws Exception{
+
+		Capacite[] testcap=new Capacite[16]; Element e; int cpt=0;
+		XmlReader reader = new XmlReader();
+		Element root = reader.parse(Gdx.files.internal("xml/Capacite.xml"));
+		Array<Element> elements= root.getChildrenByName("Atk");
+		for(Element temp:elements){
+			for(int i=0;i<temp.getChildCount();i++){
+			e=temp.getChild(i);System.out.println("temp"+temp);
+			testcap[cpt]=new Atk(e.getInt("power"),e.getInt("pre"),e.getInt("CC"),e.get("nom"),e.get("description"),
+							   Type.valueOf(e.get("element")),e.getInt("type"),e.getInt("maxPP"),Statut.valueOf(e.get("effet"))
+							  );
+			cpt++;
+			}
+		}
+	}
+	public static void main(String args[]) throws Exception{
 		MyGdxGame.initStatic();
 		Combat ctest=new Combat();
 		//ctest.combatsolo(MyGdxGame.Jtest,MyGdxGame.Ptest[5]);
 		TestCombat.fromXML();
 	} 
-	
-	
-	public static void fromXML(){
-		Capacite[] testcap=new Capacite[16]; int i=0; Element temp;
-		XmlReader reader = new XmlReader();
-		InputStream inputstream;
-		FileHandle test=Gdx.files.internal("xml/Capacite.xml");
-
-		try {
-			inputstream = new FileInputStream("C:/Users/User/git/Pokemon/Pokemon-android/assets/xml/Capacite.xml");
-			System.out.println("ICI"); 
-			Element root = reader.parse(Gdx.files.local("assets/xml/Capacite.xml"));
-			System.out.println("ICI "+root);
-			Array<Element> elements= root.getChildrenByName("Atk");
-			System.out.println("ICI");
-			for(Element e:elements){
-				System.out.println("ICI");
-				temp=e.getChild(0);System.out.println(temp);
-				testcap[i]=new Atk(e.getInt("power"),e.getInt("pre"),e.getInt("CC"),e.get("nom"),e.get("description"),
-								   Type.valueOf(e.get("element")),e.getInt("type"),e.getInt("maxPP"),Statut.valueOf(e.get("effet"))
-								  );
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		for(Capacite c:testcap){
-			System.out.println(c);
-		}
-	}
-	
 
 	
 }
