142ff845a3739ab3ce9a9d3c88da367a49a319f5
Veber Axel <axel.veber@gmail.com>
2015-03-24 18:02:46 +0100

    -JoueurVue et JoueurController sont fonctionnelles -MapScreen a fait un régime ! -Meilleur respect du pattern MVC

--- Pokemon-core/src/pokemon/controle/JoueurController.java
+++ Pokemon-core/src/pokemon/controle/JoueurController.java
@@ -1,5 +1,140 @@
 package pokemon.controle;
 
-public class JoueurController {
+import java.util.Vector;
+
+import pokemon.launcher.MapScreen;
+import pokemon.modele.Direction;
+import pokemon.modele.Joueur;
+import pokemon.vue.JoueurVue;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
+
+/* La classe JoueurController gere les inputs sur la map*/
+
+public class JoueurController implements InputProcessor{
+	private Vector<Direction> input=new Vector<Direction>();
+	private JoueurVue jv;
+	private Joueur j;
+	private MapScreen screen;
+	
+	//Constructeurs
+	public JoueurController() {
+		this.jv = new JoueurVue();
+		j = jv.getJoueur();
+		screen = new MapScreen();
+	}
+	public JoueurController(JoueurVue jv) {
+		this.jv = jv;
+		this.j = jv.getJoueur();
+		screen = new MapScreen();
+	}
+	public JoueurController(MapScreen screen, JoueurVue jv) {
+		this.jv = jv;
+		this.j = jv.getJoueur();
+		this.screen = screen;
+	}
+	
+	//Pour Desktop
+	@Override
+	public boolean keyDown(int arg0) {
+		if(!j.isTalking()) {		
+			if(Gdx.input.isKeyPressed(Keys.RIGHT))
+			{
+				if(input.size()<2 && !input.contains(Direction.East)) {
+					input.add(0, Direction.East);
+					j.move(input.firstElement());
+					jv.setAnimation(Direction.East);
+				}
+				
+			}
+			if(Gdx.input.isKeyPressed(Keys.LEFT) ){
+				
+				if(input.size()<2 && !input.contains(Direction.West)) {
+					input.add(0, Direction.West);
+					j.move(input.firstElement());
+					jv.setAnimation(Direction.West);
+				}
+			}
+			if(Gdx.input.isKeyPressed(Keys.DOWN)){
+				
+				if(input.size()<2 && !input.contains(Direction.South)) {
+					input.add(0, Direction.South);
+					j.move(input.firstElement());
+					jv.setAnimation(Direction.South);
+				}
+				
+			}
+			if(Gdx.input.isKeyPressed(Keys.UP))
+			{
+				if(input.size()<2) {
+					input.add(0, Direction.North);
+					j.move(input.firstElement());				
+					jv.setAnimation(Direction.North);
+				}
+				
+			}
+		}
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			screen.updateDialogBox(j);
+		}
+		return false;
+	}
+	@Override
+	public boolean keyUp(int arg0) {
+		switch(arg0)
+		{
+		case Keys.RIGHT:
+			input.remove(Direction.East);
+			break;
+		case Keys.LEFT:
+			input.remove(Direction.West);
+			break;
+		case Keys.DPAD_UP:
+			input.remove(Direction.North);
+			break;
+		case Keys.DPAD_DOWN:
+			input.remove(Direction.South);
+			break;
+
+		}
+		if(input.size()!=0){
+			j.move(input.firstElement());
+			jv.setAnimation(input.firstElement());
+		}
+		else 
+			j.move(Direction.Standing);
+		return false;
+	}
+	@Override
+	public boolean keyTyped(char arg0) {
+		return false;
+	}
+
+
+	//Pour Android
+	@Override
+	public boolean touchDown(int arg0, int arg1, int arg2, int arg3) {
+		return false;
+	}
+	@Override
+	public boolean touchDragged(int arg0, int arg1, int arg2) {
+		return false;
+	}
+	@Override
+	public boolean touchUp(int arg0, int arg1, int arg2, int arg3) {
+		return false;
+	}
+	
+	//A voir
+	@Override
+	public boolean mouseMoved(int arg0, int arg1) {
+		return false;
+	}	
+	@Override
+	public boolean scrolled(int arg0) {
+		return false;
+	}
 
 }
---
+++ Pokemon-core/src/pokemon/modele/ChangeMapException.java
@@ -0,0 +1,13 @@
+package pokemon.modele;
+
+public class ChangeMapException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChangeMapException() {
+		super();
+	}
+	public ChangeMapException(String s) {
+		super(s);
+	}
+}
