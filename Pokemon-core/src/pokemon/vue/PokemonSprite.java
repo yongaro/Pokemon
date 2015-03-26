f8f20ffa58c733233981512cc89514d818115f0c
flacombe <flacombe@m12.etu.info-ufr.univ-montp2.fr>
2015-03-26 10:43:17 +0100

    Sprite dresseur dans combat

--- Pokemon-core/src/pokemon/vue/PokemonSprite.java
+++ Pokemon-core/src/pokemon/vue/PokemonSprite.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 public class PokemonSprite extends Actor{
 	public static Vector2 e1=new Vector2(60,220);
 	public static Vector2 e2=new Vector2(-100,220);
-	public static Vector2 a1=new Vector2(440,50);
+	public static Vector2 a1=new Vector2(440,60);
     public Texture s=new Texture(Gdx.files.internal("Sprites/6.png"));
     SpriteBatch b=new SpriteBatch();
 	ShapeRenderer shapeRenderer=new ShapeRenderer();
@@ -27,10 +27,10 @@ public class PokemonSprite extends Actor{
     	pos=new Vector2(v);
    	   	this.setBounds(pos.x, pos.y, s.getWidth()*1.2f, s.getHeight()*1.2f);
    	   		if(pos.x<320)
-    	   	this.addAction(Actions.moveTo(pos.x+420, v.y, 2.0f));
+   	   			this.addAction(Actions.moveTo(pos.x+420, v.y, 2.0f));
    	   		else
    	    	   	this.addAction(Actions.moveTo(pos.x-420, v.y, 2.0f));
-   	   		this.addAction(Actions.scaleBy(150, 150,2.0f));
+   	   		this.addAction(Actions.scaleTo(1.5f, 1.5f,2.0f));
 	        b.setColor(0.2f, 0.2f, 0.2f,1); 
 	        b.getProjectionMatrix().setToOrtho2D(0, 0,640,360);
     }
@@ -46,15 +46,15 @@ public class PokemonSprite extends Actor{
 	     		b.setColor(Color.WHITE);
 		   System.out.print("Scale:"+this.getScaleX());
 		   System.out.print("Pos:"+this.getX()+";"+this.getY());
-	       b.draw(s,this.getX(),this.getY(),this.getScaleX(), this.getScaleY());
+	       b.draw(s,this.getX(),this.getY(),s.getWidth()*this.getScaleX(),s.getHeight()*this.getScaleY());
 	      // b.setColor(color);
 	       b.end();
 	        System.out.println(this.getX()+"+"+this.getY());
-	        shapeRenderer.setProjectionMatrix(this.getStage().getViewport().getCamera().combined);
+	       /* shapeRenderer.setProjectionMatrix(this.getStage().getViewport().getCamera().combined);
 	        shapeRenderer.begin(ShapeType.Filled);
 			shapeRenderer.rect(0, 0, 640, 100);
 			shapeRenderer.setColor(0.58f, 0.59f, 0.57f, 1);
-			shapeRenderer.end();
+			shapeRenderer.end();*/
 	    	//batch.draw(t,this.getX(),this.getY(),t.getWidth(),t.getHeight());
 	    	//batch.draw(s,this.getX()+Minimap.BourgPalette.getX(),this.getY()+Minimap.BourgPalette.getY(),30,30);
 	    }
